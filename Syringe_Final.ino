ac74e14486c381c09bbe5ac5b6afe35351f35a2e
Alan Grover <awgrover@gmail.com>
2019-11-30 10:56:47 -0500

    commented

--- Syringe_Final.ino
+++ Syringe_Final.ino
@@ -1,14 +1,18 @@
+/*
+  Run a syringe in/out using the stepper.
+  Reset button: retract and halt
+  Start button:
+*/
+
 #include <Bounce2.h>
 #include <AccelStepper.h>
 
-
 const int stepPin = 3;
 const int dirPin = 4;
-const int startPin = 5;
-const int resetPin = 2;
 const int motorInterfaceType = 1;
 
-
+const int startPin = 5; // momentary
+const int resetPin = 2; // momentary
 
 //////////////// variables to be modified as you wish later on /////////
 const int MAXSPEED = 7000;
@@ -42,26 +46,28 @@ void setup() {
   pinMode(resetPin, INPUT_PULLUP);
 
   attachInterrupt(digitalPinToInterrupt(resetPin), ResetFunc, FALLING );
+
   Serial.begin(9600);
+
   startButton.attach(startPin);
   startButton.interval(50);
+
   pinMode (stepPin, OUTPUT);
   pinMode (dirPin, OUTPUT);
   stepper.setMaxSpeed(MAXSPEED);
-  originalPos = stepper.currentPosition() ;
+  originalPos = stepper.currentPosition() ; // isn't that just 0?
+
   randomSeed(analogRead(0));
   stepsTaken = 0;
 }
 
-void ResetFunc()
-{
-
+void ResetFunc() {
 
   isStarted = false;
   isReset = true;
 
-
 }
+
 void loop() {
 
   // put your main code here, to run repeatedly:
@@ -70,15 +76,18 @@ void loop() {
   if (!isStarted)
   {
 
+    // on push, mark started
     if (startButton.fell())
     {
-
       isStarted = true;
     }
+
+    // watch for reset
     else if (isReset)
     {
       isReset = false;
 
+      // retract
       while (stepsTaken > originalPos)
       {
         stepper.setSpeed(-MAXSPEED);
@@ -92,23 +101,31 @@ void loop() {
 
 
   }
+
+  // running
   else
   {
-    randDelay = random(MINDELAY, MAXDELAY);
+    // wants some variability
+    randDelay = random(MINDELAY, MAXDELAY); // stutter possibly
     randSpeed = random(MINSPEED, MAXSPEED);
 
+    // decide which way to go, how far, and how fast
+    // We want a big step (between MINSTEP & MAXSTEP),
+    // tending towards push (3/4 of the time)
     do {
+      // guess a distance and direction...
+      randSteps = random(MINSTEP, MAXSTEP); // the goal distance
+      randDir = random(-1, 2); // -1=retract, 0,1=push x1, 2=push x2
+      if (randDir == 0) randDir++;
+      // try again if that would retract too far (<originalPos)
+      // or push too far (>STEPSLIMIT)
+    } while ((stepsTaken + (randSteps * randDir) < originalPos) || stepsTaken + (randSteps * randDir) > STEPSLIMIT);
 
-      randSteps = random(MINSTEP, MAXSTEP);
-      randDir = random(-1, 2);
-      if (randDir == 0)randDir++;
-    }    while ((stepsTaken + (randSteps * randDir) < originalPos) || stepsTaken + (randSteps * randDir) > STEPSLIMIT);
-
-
+    // remember, a "reset" could during here
+    // step for the distance
     while  (isStarted && stepper.currentPosition() != ( (randSteps * randDir) ))
     {
-
-      stepper.setSpeed(randSpeed * randDir);
+      stepper.setSpeed(randSpeed * randDir); // that seems unlikely, already near MAXSPEED?
       stepper.runSpeed();
       delay (randDelay);
       stepsTaken += randDir;
@@ -117,8 +134,6 @@ void loop() {
     }
     stepper.setCurrentPosition(0);
 
-
   }
-
 
 }
